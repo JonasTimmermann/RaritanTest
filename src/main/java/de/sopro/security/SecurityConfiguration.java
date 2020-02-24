e2b25cfa08f3521151e9bd0079f47cc2a8ea2e05
Jonas Timmermann <36668988+JonasTimmermann@users.noreply.github.com>
2020-02-24 02:10:21 +0100

    gg

--- src/main/java/de/sopro/security/SecurityConfiguration.java
+++ src/main/java/de/sopro/security/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().cors().configurationSource(corsConfigurationSource()).and().antMatcher("/**").authorizeRequests()
+		http.authorizeRequests()
 				.antMatchers("/", "/images/*", "/css/*", "/webjars/**").permitAll()
 				.antMatchers("/hello/*", "/search", "/api/search/*").permitAll()
 				.anyRequest()
